d23b988b932d1e93c935a5d86d42b0bd6e8412e7
unknown <kkunut.a@hotmail.com>
2021-07-01 21:16:11 +0700

    test

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -63,6 +63,7 @@ exports.signup = (req, res) => {
 };
 
 exports.signin = (req, res) => {
+  console.log(req.body)
   User.findOne({
     username: req.body.username
   })
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:3000"
 };
 
 app.use(cors(corsOptions));
